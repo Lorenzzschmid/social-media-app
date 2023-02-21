7d71d73ea11c201747e5dd433cf531138ea5feb8
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-02-21 22:41:54 +0100

    database connection

--- server/index.js
+++ server/index.js
@@ -5,6 +5,21 @@ import cors from 'cors';
 
 const app = express(); 
 
+app.use(express.json()); 
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true })); 
 app.use(cors()); 
+
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+    )
+    .then(()=> {
+        console.log("Database connected! 😃");
+    })
+    .catch((error) => {
+        console.log(error.message);
+        console.log("🤨")
+    }); 
+//Routers

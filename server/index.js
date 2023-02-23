d2cf2fb30830009b7bd2773258da0c156709f5b5
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-02-23 14:05:31 +0100

    setting mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express'; 
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv'; 
 import mongoose from 'mongoose'; 
 import cors from 'cors'; 
+
+//router
+
+dotenv.config();
 
 const app = express(); 
 
@@ -13,10 +18,19 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000; 
 
+const CONNECTION_URL = 'mongodb+srv://lo:<password>@cluster0.zj6f5cc.mongodb.net/test'; 
+
+// mongoose
+//     .connect(
+//         `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`, 
+//         { useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+//     .catch((error) => console.log(error.message)); 
+
+// mongoose.set('useFindAndModify', false); 
+
 mongoose
-    .connect(
-        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`, 
-        { useNewUrlParser: true, useUnifiedTopology: true})
+    .connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((error) => console.log(error.message)); 
 

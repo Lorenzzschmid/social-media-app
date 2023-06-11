3cad4161ec77ab5e19f3a5f8669760fcf00eebbd
Lorenzzschmid <lorenz-schmid@gmx.net>
2023-06-11 21:29:41 +0200

    serverjs edit

--- server/server.js
+++ server/server.js
@@ -3,3 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose'; 
 import cors from 'cors'; 
 
+const app = express(); 
+app.use(express.json());
+app.use(cors({origin: true})); 
+
